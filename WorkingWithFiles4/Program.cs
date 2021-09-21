ceb057056f024e26790897d0d9da8b17d26df5ba
Lucas Ortis <lucasortis01@gmail.com>
2021-09-21 19:42:53 -0300

    Initial project

---
+++ WorkingWithFiles4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkingWithFiles4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
